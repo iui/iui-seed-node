391d83086de903db0ca33f550b9cb12d9f1acfa1
Michael Neale <michael.neale@gmail.com>
2013-05-06 11:32:22 +1000

    changes to make it a clickstart

--- node-deploy-cloudbees.sh
+++ node-deploy-cloudbees.sh
@@ -5,5 +5,5 @@ rm -f target/node-web-app.zip
 cd web-app
 zip -r ../target/node-web-app.zip *
 cd ..
-bees app:deploy --appid node-seed -t node -RPLUGIN.SRC.node=https://s3.amazonaws.com/cloudbees-downloads/clickstack/node-clickstack.zip target/node-web-app.zip
+bees app:deploy --appid node-seed -t node -RPLUGIN.SRC.node=https://s3.amazonaws.com/clickstacks/admin/nodejs-plugin-0.10.5.zip target/node-web-app.zip
 
